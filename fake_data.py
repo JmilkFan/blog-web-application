800a09900ba1144bc10ca071147fad09710bbc25
JmilkFan <fangui_ju@163.com>
2016-12-11 11:23:27 +0800

    Fix some bug

--- fake_data.py
+++ fake_data.py
@@ -2,27 +2,20 @@ import random
 import datetime
 from uuid import uuid4
 
-from jmilkfansblog.models import db, User, Tag, Post, Role
+from jmilkfansblog.models import db, User, Post, Role, BrowseVolume
 
-user = User(id=str(uuid4()), username='jmilkfan', password='fanguiju')
-db.session.add(user)
+admin_role = Role(id=str(uuid4()), name='admin')
+poster_role = Role(id=str(uuid4()), name='poster')
+default_role = Role(id=str(uuid4()), name='default')
+
+browse_volume = BrowseVolume(id=str(uuid4()))
+
+db.session.add(default_role)
+db.session.add(browse_volume)
 db.session.commit()
 
-user = db.session.query(User).first()
-tag_one = Tag(id=str(uuid4()), name='Python')
-tag_two = Tag(id=str(uuid4()), name='Flask')
-tag_three = Tag(id=str(uuid4()), name='SQLALchemy')
-tag_four = Tag(id=str(uuid4()), name='JMilkFan')
-tag_list = [tag_one, tag_two, tag_three, tag_four]
+admin_user = User(id=str(uuid4()), username='jmilkfan', password='fanguiju2016.com')
+admin_user.roles = [admin_role, poster_role, default_role]
 
-s = "EXAMPLE TEXT"
-
-for i in xrange(100):
-    new_post = Post(id=str(uuid4()), title="Post" + str(i))
-    new_post.user = user
-    new_post.publish_date = datetime.datetime.now()
-    new_post.text = s
-    new_post.tags = random.sample(tag_list, random.randint(1, 3))
-    db.session.add(new_post)
-
+db.session.add(admin_user)
 db.session.commit()
--- migrations/versions/49c7263e04a8_add_table_of_browse_volumes.py
+++ migrations/versions/49c7263e04a8_add_table_of_browse_volumes.py
@@ -19,7 +19,7 @@ depends_on = None
 def upgrade():
     op.create_table('browse_volumes',
     sa.Column('id', sa.String(length=45), nullable=False),
-    sa.Column('name', sa.Integer, nullable=False),
+    sa.Column('home_view_total', sa.Integer, nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
 
